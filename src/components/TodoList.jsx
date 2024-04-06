175e97ad89f623df7f43da75b71edc62115e6e53
crosskicker <alban.personnaz@gmail.com>
2024-04-06 15:43:19 +0200

    modification todo pas fini

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,15 +1,33 @@
 import TodoItem from './TodoItem';
+import EditTodo from './EditTodo';
 
-export default function TodoList({ todoList, deleteTodo }) {
+export default function TodoList({
+  todoList,
+  deleteTodo,
+  toggleTodo,
+  toggleTodoEdit,
+  editTodo,
+}) {
   return todoList.length ? (
     <ul>
-      {todoList.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          deleteTodo={() => deleteTodo(todo.id)}
-        />
-      ))}
+      {todoList.map((todo) =>
+        todo.edit ? (
+          <EditTodo
+            key={todo.id}
+            todo={todo}
+            editTodo={(content) => editTodo(todo.id, content)}
+            cancelEditTodo={() => toggleTodoEdit(todo.id)}
+          />
+        ) : (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            deleteTodo={() => deleteTodo(todo.id)}
+            toggleTodo={() => toggleTodo(todo.id)}
+            editTodo={() => toggleTodoEdit(todo.id)}
+          />
+        )
+      )}
     </ul>
   ) : (
     <p>Aucune tâche en cours </p>
