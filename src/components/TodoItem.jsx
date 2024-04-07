bd3e40e98652ab55b2df32bb0ff0b12a39143915
crosskicker <alban.personnaz@gmail.com>
2024-04-07 15:38:08 +0200

    usecontext

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,47 +1,45 @@
+import Button from './Button';
+
 export default function TodoItem({
-    todo,
-    deleteTodo,
-    toggleTodo,
-    editTodo,
-    selectTodo,
-  }) {
-    return (
-      <li
-        onClick={selectTodo}
-        className={`mb-10 d-flex flex-row justify-content-center align-items-center p-10 ${
-          todo.selected ? 'selected' : ''
-        }  `}
-      >
-        <span className="flex-fill">
-          {todo.content} {todo.done && '✅'}
-        </span>
-        <button
-          className="btn btn-primary mr-15"
-          onClick={(e) => {
-            e.stopPropagation();
-            toggleTodo();
-          }}
-        >
-          Valider
-        </button>
-        <button
-          className="btn btn-primary mr-15"
-          onClick={(e) => {
-            e.stopPropagation();
-            editTodo();
-          }}
-        >
-          Modifier
-        </button>
-        <button
-          className="btn btn-reverse-primary"
-          onClick={(e) => {
-            e.stopPropagation();
-            deleteTodo();
-          }}
-        >
-          Supprimer
-        </button>
-      </li>
-    );
-  }
+  todo,
+  deleteTodo,
+  toggleTodo,
+  editTodo,
+  selectTodo,
+}) {
+  return (
+    <li
+      onClick={selectTodo}
+      className={`mb-10 d-flex flex-row justify-content-center align-items-center p-10 ${
+        todo.selected ? 'selected' : ''
+      }  `}
+    >
+      <span className="flex-fill">
+        {todo.content} {todo.done && '✅'}
+      </span>
+      <Button
+        text="Valider"
+        className="mr-15"
+        onClick={(e) => {
+          e.stopPropagation();
+          toggleTodo();
+        }}
+      />
+      <Button
+        text="Modifier"
+        className="mr-15"
+        onClick={(e) => {
+          e.stopPropagation();
+          editTodo();
+        }}
+      />
+      <Button
+        text="Supprimer"
+        onClick={(e) => {
+          e.stopPropagation();
+          deleteTodo();
+        }}
+      />
+    </li>
+  );
+}
